099d5c0140342c00f69a983fad6a4dcba627f45c
Xei201 <xei201@yandex.ru>
2023-04-04 11:32:50 +0300

    Корректировка моделей

--- uptrader/menu/models.py
+++ uptrader/menu/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 
 
 class Menu(models.Model):
+    """Определеяет список меню"""
     name_menu = models.CharField(primary_key=True,
                                  verbose_name='Menu name',
                                  max_length=255)
@@ -15,10 +16,13 @@ class Menu(models.Model):
 
 
 class TreeMenu(models.Model):
+    """Определяет итемы меню и связь между ними"""
+
     name_tree = models.CharField(verbose_name='Tree name', max_length=255)
 
     menu = models.ForeignKey(Menu,
                              verbose_name='Menu name',
+                             blank=True,
                              on_delete=models.CASCADE)
 
     parent = models.ForeignKey('self',
@@ -32,7 +36,17 @@ class TreeMenu(models.Model):
         ordering = ["pk"]
 
     def get_absolute_url(self):
+        """Возвращает ссылки на разделы меню с параметрами"""
         return reverse('general') + f'?menu={str(self.menu)}&pk={str(self.pk)}'
+
+    def save(self, *args, **kwargs):
+        """
+        Устанавливает в поле меню детелей параметры из
+        родителя для корректной рабоыт отображжения меню
+        """
+        if self.parent:
+            self.menu = self.parent.menu
+        super(TreeMenu, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name_tree
